3a548d6b108846acc1d82f8015096a307334cf71
Kumar Yadav <Adarsh.KumarYadav@dell.com>
2023-08-11 10:35:36 +0530

    Format fix
    Signed-off-by: Kumar Yadav <Adarsh.KumarYadav@dell.com>

--- pkg/testcore/suites/perf-suites.go
+++ pkg/testcore/suites/perf-suites.go
@@ -1267,17 +1267,17 @@ func (vgs *VolumeGroupSnapSuite) GetClients(namespace string, client *k8sclient.
 	if vaErr != nil {
 		return nil, vaErr
 	}
-	
+
 	metricsClient, mcErr := client.CreateMetricsClient(namespace)
 	if mcErr != nil {
 		return nil, mcErr
 	}
-		
+
 	return &k8sclient.Clients{
-		PVCClient: pvcClient,
-		PodClient: podClient,
-		VaClient:  vaClient,
-		VgsClient: vgsClient,
+		PVCClient:     pvcClient,
+		PodClient:     podClient,
+		VaClient:      vaClient,
+		VgsClient:     vgsClient,
 		MetricsClient: metricsClient,
 	}, nil
 }
