ea003a58865fd8d8d04991ec053e1c15525e77b1
meghana_gm <meghana.gm@dell.com>
2025-02-11 13:45:28 +0530

    UT coverage
    Signed-off-by: meghana_gm <meghana.gm@dell.com>

--- pkg/k8sclient/resources/pvc/pvc_test.go
+++ pkg/k8sclient/resources/pvc/pvc_test.go
@@ -21,8 +21,10 @@ import (
 	"fmt"
 	"github.com/dell/cert-csi/pkg/k8sclient"
 	pvc2 "github.com/dell/cert-csi/pkg/k8sclient/resources/pvc"
+	"github.com/dell/cert-csi/pkg/k8sclient/resources/sc"
 	"github.com/stretchr/testify/suite"
 	v1 "k8s.io/api/core/v1"
+	v2 "k8s.io/api/storage/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
@@ -242,6 +244,177 @@ func (suite *PVCTestSuite) TestDeleteAll() {
 	}
 }
 
+func (suite *PVCTestSuite) TestWaitForAllToBeBound() {
+	ctx := context.Background()
+
+	// Create a list of PVCs to be returned by the fake client
+	pvcList := &v1.PersistentVolumeClaimList{
+		Items: []v1.PersistentVolumeClaim{
+			{ObjectMeta: metav1.ObjectMeta{Name: "pvc-1"}, Status: v1.PersistentVolumeClaimStatus{Phase: v1.ClaimPending}},
+			{ObjectMeta: metav1.ObjectMeta{Name: "pvc-2"}, Status: v1.PersistentVolumeClaimStatus{Phase: v1.ClaimPending}},
+			{ObjectMeta: metav1.ObjectMeta{Name: "pvc-3"}, Status: v1.PersistentVolumeClaimStatus{Phase: v1.ClaimPending}},
+		},
+	}
+
+	// Mock the List function to return the PVC list
+	suite.kubeClient.ClientSet.(*fake.Clientset).PrependReactor("list", "persistentvolumeclaims", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		return true, pvcList, nil
+	})
+
+	// Mock the List function to simulate PVCs becoming bound
+	suite.kubeClient.ClientSet.(*fake.Clientset).PrependReactor("list", "persistentvolumeclaims", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		for i := range pvcList.Items {
+			pvcList.Items[i].Status.Phase = v1.ClaimBound
+		}
+		return true, pvcList, nil
+	})
+	pvcClient, err := suite.kubeClient.CreatePVCClient("default")
+	suite.NoError(err)
+	// Call the WaitForAllToBeBound function
+	err = pvcClient.WaitForAllToBeBound(ctx)
+	suite.NoError(err)
+}
+
+func (suite *PVCTestSuite) TestWaitForAllToBeBound_Error() {
+	ctx := context.Background()
+
+	// Mock the List function to return an error
+	suite.kubeClient.ClientSet.(*fake.Clientset).PrependReactor("list", "persistentvolumeclaims", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		return true, nil, fmt.Errorf("failed to list PVCs")
+	})
+	pvcClient, err := suite.kubeClient.CreatePVCClient("default")
+	suite.NoError(err)
+	// Call the WaitForAllToBeBound function
+	err = pvcClient.WaitForAllToBeBound(ctx)
+	suite.Error(err)
+	suite.EqualError(err, "failed to list PVCs")
+}
+
+func (suite *PVCTestSuite) TestCheckAnnotationsForVolumes_Error() {
+	ctx := context.Background()
+
+	// Create a mock StorageClass object
+	scObject := &v2.StorageClass{
+		Parameters: map[string]string{
+			sc.RemoteClusterID:        "remote-cluster-id",
+			sc.RemoteStorageClassName: "remote-storage-class-name",
+		},
+	}
+
+	// Mock the List function to return an error
+	suite.kubeClient.ClientSet.(*fake.Clientset).PrependReactor("list", "persistentvolumeclaims", func(action testing.Action) (handled bool, ret runtime.Object, err error) {
+		return true, nil, fmt.Errorf("failed to list PVCs")
+	})
+	pvcClient, err := suite.kubeClient.CreatePVCClient("default")
+	suite.NoError(err)
+	// Call the CheckAnnotationsForVolumes function
+	err = pvcClient.CheckAnnotationsForVolumes(ctx, scObject)
+	suite.Error(err)
+	suite.EqualError(err, "failed to list PVCs")
+}
+
+func (suite *PVCTestSuite) TestCheckAnnotationsForVolumes_ContextCancelled() {
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel() // Cancel the context immediately
+
+	// Create a mock StorageClass object
+	scObject := &v2.StorageClass{
+		Parameters: map[string]string{
+			sc.RemoteClusterID:        "remote-cluster-id",
+			sc.RemoteStorageClassName: "remote-storage-class-name",
+		},
+	}
+	pvcClient, err := suite.kubeClient.CreatePVCClient("default")
+	suite.NoError(err)
+	// Call the CheckAnnotationsForVolumes function
+	err = pvcClient.CheckAnnotationsForVolumes(ctx, scObject)
+	suite.Error(err)
+	suite.EqualError(err, "stopped waiting for annotations and labels")
+}
+
+func (suite *PVCTestSuite) TestCreatePVCObject() {
+	ctx := context.Background()
+
+	// Create a mock PersistentVolume object
+	remotePVObject := &v1.PersistentVolume{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "pv-1",
+			Annotations: map[string]string{
+				"replication.storage.dell.com/remotePVC":              "pvc-1",
+				"replication.storage.dell.com/resourceRequest":        `{"storage":"10Gi"}`,
+				"replication.storage.dell.com/remoteClusterID":        "remote-cluster-id",
+				"replication.storage.dell.com/remoteStorageClassName": "remote-storage-class-name",
+			},
+			Labels: map[string]string{
+				"replication.storage.dell.com/some-label": "some-value",
+			},
+		},
+		Spec: v1.PersistentVolumeSpec{
+			StorageClassName: "standard",
+			AccessModes:      []v1.PersistentVolumeAccessMode{v1.ReadWriteOnce},
+			VolumeMode:       func() *v1.PersistentVolumeMode { mode := v1.PersistentVolumeFilesystem; return &mode }(),
+		},
+	}
+
+	remoteNamespace := "remote-namespace"
+	pvcClient, err := suite.kubeClient.CreatePVCClient("default")
+	suite.NoError(err)
+	// Call the CreatePVCObject function
+	pvcObject := pvcClient.CreatePVCObject(ctx, remotePVObject, remoteNamespace)
+
+	// Verify the PVC object
+	suite.Equal("pvc-1", pvcObject.Name)
+	suite.Equal(remoteNamespace, pvcObject.Namespace)
+	suite.Equal("remote-cluster-id", pvcObject.Annotations["replication.storage.dell.com/remoteClusterID"])
+	suite.Equal("remote-storage-class-name", pvcObject.Annotations["replication.storage.dell.com/remoteStorageClassName"])
+	suite.Equal("some-value", pvcObject.Labels["replication.storage.dell.com/some-label"])
+	suite.Equal("standard", *pvcObject.Spec.StorageClassName)
+	suite.Equal([]v1.PersistentVolumeAccessMode{v1.ReadWriteOnce}, pvcObject.Spec.AccessModes)
+	suite.Equal(v1.PersistentVolumeFilesystem, *pvcObject.Spec.VolumeMode)
+	//suite.Equal("10Gi", pvcObject.Spec.Resources.Requests["storage"])
+	suite.Equal("pv-1", pvcObject.Spec.VolumeName)
+}
+func (suite *PVCTestSuite) TestCreatePVCObject_Error() {
+	ctx := context.Background()
+
+	// Create a mock PersistentVolume object with invalid resourceRequest
+	remotePVObject := &v1.PersistentVolume{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "pv-1",
+			Annotations: map[string]string{
+				"replication.storage.dell.com/remotePVC":              "pvc-1",
+				"replication.storage.dell.com/resourceRequest":        `invalid-json`,
+				"replication.storage.dell.com/remoteClusterID":        "remote-cluster-id",
+				"replication.storage.dell.com/remoteStorageClassName": "remote-storage-class-name",
+			},
+			Labels: map[string]string{
+				"replication.storage.dell.com/some-label": "some-value",
+			},
+		},
+		Spec: v1.PersistentVolumeSpec{
+			StorageClassName: "standard",
+			AccessModes:      []v1.PersistentVolumeAccessMode{v1.ReadWriteOnce},
+			VolumeMode:       func() *v1.PersistentVolumeMode { mode := v1.PersistentVolumeFilesystem; return &mode }(),
+		},
+	}
+
+	remoteNamespace := "remote-namespace"
+	pvcClient, err := suite.kubeClient.CreatePVCClient("default")
+	suite.NoError(err)
+	// Call the CreatePVCObject function
+	pvcObject := pvcClient.CreatePVCObject(ctx, remotePVObject, remoteNamespace)
+
+	// Verify the PVC object is empty due to the error
+	suite.Empty(pvcObject.Name)
+	suite.Empty(pvcObject.Namespace)
+	suite.Empty(pvcObject.Annotations)
+	suite.Empty(pvcObject.Labels)
+	suite.Empty(pvcObject.Spec.StorageClassName)
+	suite.Empty(pvcObject.Spec.AccessModes)
+	suite.Empty(pvcObject.Spec.VolumeMode)
+	suite.Empty(pvcObject.Spec.Resources.Requests)
+	suite.Empty(pvcObject.Spec.VolumeName)
+}
 func TestPVCSuite(t *t.T) {
 	suite.Run(t, new(PVCTestSuite))
 }
