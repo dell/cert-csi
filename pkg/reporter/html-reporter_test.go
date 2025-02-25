d1f76f2a144ee61c2a525e77d8c2332826834079
Sahiba_G <Sahiba.gupta1@dell.com>
2025-02-25 16:55:13 +0000

    Add html reporter test file

---
+++ pkg/reporter/html-reporter_test.go
@@ -0,0 +1,42 @@
+package reporter
+
+import (
+	"testing"
+
+	"github.com/dell/cert-csi/pkg/collector"
+)
+
+func TestHTMLReporter_Generate(t *testing.T) {
+	tests := []struct {
+		name    string
+		runName string
+		mc      *collector.MetricsCollection
+		wantErr bool
+	}{
+		{
+			name:    "successful report generation",
+			runName: "test_run",
+			mc:      &collector.MetricsCollection{
+				// fill in with test data
+			},
+			wantErr: false,
+		},
+		{
+			name:    "error during report generation",
+			runName: "test_run",
+			mc:      nil,
+			wantErr: true,
+		},
+		// add more test cases with different scenarios
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			hr := &HTMLReporter{}
+			err := hr.Generate(tt.runName, tt.mc)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("HTMLReporter.Generate() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
