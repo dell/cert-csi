e04e3a875254ab4d99913b597683a2bdeb6b3acb
aqu-dell <arnchie.qu@dell.com>
2025-03-05 00:40:23 +0000

    va test increase

--- pkg/observer/va_alt_test.go
+++ pkg/observer/va_alt_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/dell/cert-csi/pkg/k8sclient"
+	"github.com/dell/cert-csi/pkg/k8sclient/resources/va"
 	"github.com/dell/cert-csi/pkg/store"
 	storagev1 "k8s.io/api/storage/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -17,7 +18,6 @@ import (
 )
 
 func TestVaListObserver_StartWatching(t *testing.T) {
-	// Test case: Watching volume attachments
 	ctx := context.Background()
 
 	storageClass := &storagev1.StorageClass{
@@ -28,93 +28,174 @@ func TestVaListObserver_StartWatching(t *testing.T) {
 		}(),
 	}
 	clientSet := NewFakeClientsetWithRestClient(storageClass)
-
-	va := &storagev1.VolumeAttachment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-va",
-			UID:  "test-uid",
-		},
-		Spec: storagev1.VolumeAttachmentSpec{
-			Source: storagev1.VolumeAttachmentSource{
-				PersistentVolumeName: func() *string { s := "test-pvc"; return &s }(),
-			},
-		},
-	}
-
-	/*va2 := &storagev1.VolumeAttachment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-va2",
-		},
-		Spec: storagev1.VolumeAttachmentSpec{
-			Source: storagev1.VolumeAttachmentSource{
-				PersistentVolumeName: func() *string { s := "test-pvc2"; return &s }(),
-			},
-		},
-	}*/
-
-	va3 := &storagev1.VolumeAttachment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "test-va3",
-			UID:  "test-uid3",
-		},
-		Spec: storagev1.VolumeAttachmentSpec{
-			Source: storagev1.VolumeAttachmentSource{
-				PersistentVolumeName: func() *string { s := "test-pvc3"; return &s }(),
-			},
-		},
-	}
-
-	clientSet.StorageV1().VolumeAttachments().Create(ctx, va, metav1.CreateOptions{})
-	//clientSet.StorageV1().VolumeAttachments().Create(ctx, va2, metav1.CreateOptions{})
-	clientSet.StorageV1().VolumeAttachments().Delete(ctx, "test-va", metav1.DeleteOptions{})
-	clientSet.StorageV1().VolumeAttachments().Update(ctx, va, metav1.UpdateOptions{})
-	clientSet.StorageV1().VolumeAttachments().Create(ctx, va, metav1.CreateOptions{})
-	clientSet.StorageV1().VolumeAttachments().Create(ctx, va3, metav1.CreateOptions{})
+	clientSet.StorageV1().StorageClasses().Create(ctx, storageClass, metav1.CreateOptions{})
 
 	kubeClient := &k8sclient.KubeClient{
 		ClientSet: clientSet,
 		Config:    &rest.Config{},
 	}
 
-	vaClient, _ := kubeClient.CreateVaClient("test-namespace")
-	runner := &Runner{
-		Clients: &k8sclient.Clients{
-			VaClient: vaClient,
+	pvName := "test-pv"
+	deletionVA := &storagev1.VolumeAttachment{
+		Spec: storagev1.VolumeAttachmentSpec{
+			Source: storagev1.VolumeAttachmentSource{
+				PersistentVolumeName: func() *string { s := pvName; return &s }(),
+			},
 		},
-		TestCase: &store.TestCase{
-			ID: 1,
+		Status: storagev1.VolumeAttachmentStatus{
+			Attached: false,
 		},
-		WaitGroup: sync.WaitGroup{},
-		PvcShare:  sync.Map{},
-		Database:  NewSimpleStore(),
+		ObjectMeta: metav1.ObjectMeta{
+			DeletionTimestamp: &metav1.Time{Time: time.Now()},
+			Name:              "test-volume-attachment",
+		},
 	}
-	runner.PvcShare.Store("test-pvc", &store.Entity{
-		Name: "test-pvc",
-	})
-	/*runner.PvcShare.Store("test-pvc2", &store.Entity{
-		Name: "test-pvc2",
-	})*/
-	runner.PvcShare.Store("test-pvc2", &store.Entity{
-		Name: "test-pvc2",
-	})
-	runner.WaitGroup.Add(1)
+	attachedVA := &storagev1.VolumeAttachment{
+		Spec: storagev1.VolumeAttachmentSpec{
+			Source: storagev1.VolumeAttachmentSource{
+				PersistentVolumeName: func() *string { s := pvName; return &s }(),
+			},
+		},
+		Status: storagev1.VolumeAttachmentStatus{
+			Attached: true,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-volume-attachment-2",
+		},
+	}
+	vaToBeDeleted := &storagev1.VolumeAttachment{
+		Spec: storagev1.VolumeAttachmentSpec{
+			Source: storagev1.VolumeAttachmentSource{
+				PersistentVolumeName: func() *string { s := "vaToBeDeleted"; return &s }(),
+			},
+		},
+		Status: storagev1.VolumeAttachmentStatus{
+			Attached: true,
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-volume-attachment-vaToBeDeleted",
+		},
+	}
 
-	obs := &VaListObserver{}
-	obs.MakeChannel()
+	vaClient, _ := kubeClient.CreateVaClient("test-namespace")
+	vaClient.Interface.Create(ctx, deletionVA, metav1.CreateOptions{})
+	vaClient.Interface.Create(ctx, attachedVA, metav1.CreateOptions{})
+	vaClient.Interface.Create(ctx, vaToBeDeleted, metav1.CreateOptions{})
 
-	go obs.StartWatching(ctx, runner)
+	var pvcShare sync.Map
+	pvcShare.Store(pvName, &store.Entity{})
+	pvcShare.Store("vaToBeDeleted", &store.Entity{})
 
-	clientSet.StorageV1().VolumeAttachments().Update(ctx, va, metav1.UpdateOptions{})
-	clientSet.StorageV1().VolumeAttachments().Create(ctx, va, metav1.CreateOptions{})
+	tests := []struct {
+		name                string
+		runner              *Runner
+		shouldAddedVABeTrue *bool
+	}{
+		{
+			name: "Test case: nil vaClient",
+			runner: &Runner{
+				Clients: &k8sclient.Clients{
+					VaClient: nil,
+				},
+				Database: NewSimpleStore(),
+				PvcShare: pvcShare,
+				TestCase: &store.TestCase{
+					ID: 1,
+				},
+				WaitGroup: sync.WaitGroup{},
+			},
+			shouldAddedVABeTrue: nil,
+		},
+		{
+			name: "Test case: vaClient with original addedVA",
+			runner: &Runner{
+				Clients: &k8sclient.Clients{
+					VaClient: vaClient,
+				},
+				Database:    NewSimpleStore(),
+				PvcShare:    pvcShare,
+				ShouldClean: false,
+				TestCase: &store.TestCase{
+					ID: 1,
+				},
+				WaitGroup: sync.WaitGroup{},
+			},
+			shouldAddedVABeTrue: nil,
+		},
+		{
+			name: "Test case: vaClient with mocked addedVA and attached VA and deletion VA",
+			runner: &Runner{
+				Clients: &k8sclient.Clients{
+					VaClient: vaClient,
+				},
+				Database:    NewSimpleStore(),
+				PvcShare:    pvcShare,
+				ShouldClean: false,
+				TestCase: &store.TestCase{
+					ID: 1,
+				},
+				WaitGroup: sync.WaitGroup{},
+			},
+			shouldAddedVABeTrue: func() *bool { b := true; return &b }(),
+		},
+		{
+			name: "Test case: vaClient with mocked addedVA and non-attached VA",
+			runner: &Runner{
+				Clients: &k8sclient.Clients{
+					VaClient: vaClient,
+				},
+				Database:    NewSimpleStore(),
+				PvcShare:    pvcShare,
+				ShouldClean: false,
+				TestCase: &store.TestCase{
+					ID: 1,
+				},
+				WaitGroup: sync.WaitGroup{},
+			},
+			shouldAddedVABeTrue: func() *bool { b := true; return &b }(),
+		},
+	}
 
-	time.Sleep(100 * time.Millisecond)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			var vaoFinishedWg sync.WaitGroup
+			test.runner.WaitGroup.Add(1)
 
-	obs.StopWatching()
+			vao := &VaListObserver{}
+			vao.MakeChannel()
+			// We want to run through pollImmediate at least twice
+			pollRunCount := 0
 
-	runner.WaitGroup.Wait()
+			originalGetBoolValueFromMapWithKey := getBoolValueFromMapWithKey
+			getBoolValueFromMapWithKey = func(m map[string]bool, key string) bool {
+				pollRunCount++
+				// After a couple runs, we test new state by deleting a new VA
+				if pollRunCount == 6 {
+					vaClient.Interface.Delete(ctx, vaToBeDeleted.Name, metav1.DeleteOptions{})
+				}
+				// After a couple more runs, we mark as complete
+				if pollRunCount == 12 {
+					vaoFinishedWg.Done()
+				}
+				if test.shouldAddedVABeTrue != nil {
+					return *test.shouldAddedVABeTrue
+				} else {
+					return originalGetBoolValueFromMapWithKey(m, key)
+				}
+			}
+			defer func() {
+				getBoolValueFromMapWithKey = originalGetBoolValueFromMapWithKey
+			}()
 
-	// Assert that the function completed successfully
-	assert.True(t, true)
+			go vao.StartWatching(ctx, test.runner)
+			if test.runner.Clients.VaClient != nil {
+				vaoFinishedWg.Add(1)
+				vaoFinishedWg.Wait()
+				vao.finished <- true
+			}
+			test.runner.WaitGroup.Wait()
+		})
+	}
 }
 
 func TestVaListObserver_StopWatching(t *testing.T) {
@@ -157,6 +238,7 @@ func TestVaListObserver_MakeChannel(t *testing.T) {
 
 // Mock implementation of VaClient
 type mockVAClient struct {
+	va.Client
 	mock.Mock
 }
 
