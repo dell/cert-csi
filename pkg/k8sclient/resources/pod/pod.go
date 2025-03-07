d676563a45be63f98c3a1a90152bb1f914f1c4dc
santhoshatdell <Santhosh.Lakshmanan@dell.com>
2025-03-07 16:15:02 +0000

    Fix errors

--- pkg/k8sclient/resources/pod/pod.go
+++ pkg/k8sclient/resources/pod/pod.go
@@ -293,7 +293,6 @@ func (c *Client) Exec(ctx context.Context, pod *v1.Pod, command []string, stdout
 		TTY:       true,
 	}, scheme.ParameterCodec)
 
-	//return c.RemoteExecutor.Execute("POST", req.URL(), c.Config, nil, stdout, stderr, false, nil)
 	return executor.Execute("POST", req.URL(), c.Config, nil, stdout, stderr, false, nil)
 }
 
@@ -769,11 +768,6 @@ func (c *commandExecutor) Execute(name string, arg ...string) ([]byte, error) {
 func (c *Client) IsOCP() (bool, error) {
 	isOCP := false
 	// Run the command and capture the output
-	/*output, err := c.LocalExecutor.Execute("oc", "get", "clusterversion")
-	if err != nil {
-		// Return false and the error encountered while executing the command
-		return false, err
-	}*/
 	cmd := exec.Command("oc", "get", "clusterversion")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
